59b867f1dcd7a4a594fab41734be6e41f95dc89f
Shoaib Burq <saburq@gmail.com>
2011-03-29 21:45:23 +0200

    Update to spec.

--- spec/hot_kit_generator_spec.rb
+++ spec/hot_kit_generator_spec.rb
@@ -5,9 +5,9 @@ describe "HotKitGenerator" do
     @root_dir = "tmp/test_kit"
     @dir_config = 'lib/meta/directory_structure.yml'
     if File.exists? @root_dir
-      puts "#{'='*80}"
+      puts '='*80
       puts "** UNABLE TO RUN SPEC **\n>> Please remove '#{@root_dir}' before running the specs <<"
-      puts "#{'='*80}"
+      puts '='*80
       exit
     end
   end
